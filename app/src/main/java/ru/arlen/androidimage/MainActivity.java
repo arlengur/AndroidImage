47c0883a05872881711bdf4391558dd7fb976220
angalin <ANGalin.SBT@sberbank.ru>
2018-12-18 10:59:54 +0300

    Initial commit

---
+++ app/src/main/java/ru/arlen/androidimage/MainActivity.java
@@ -0,0 +1,20 @@
+package ru.arlen.androidimage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.main_frame, new BitmapFragment())
+                    .commit();
+        }
+    }
+}
